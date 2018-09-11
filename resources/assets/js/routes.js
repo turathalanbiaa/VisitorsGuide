30fcccf7a904f0bf64e34bd7e4d83fc90af32182
FadhilDev <fadhilabbash@gmail.com>
2018-09-11 11:24:32 +0300

    modefy router

---
+++ resources/assets/js/routes.js
@@ -0,0 +1,7 @@
+import LostIndex from './components/LostsIndex';
+import FindCenter from './components/FindCenter';
+
+export const routes = [
+    { path: '/',component:LostIndex },
+    { path: '/find',component: FindCenter },
+    ];
