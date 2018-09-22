7d334bd77824308366592f955d8add2dd316bb10
EmadAlkabi <emadalkabi1994@gmail,com>
2018-09-22 21:05:36 +0300

    modified file  word in english language

--- resources/lang/en/words.php
+++ resources/lang/en/words.php
@@ -9,11 +9,13 @@
 return [
     //App
     "app_name" => "Visitor Bag",
+
     //Select Language
     "select_language" => "Select Language",
     "arabic"          => "Arabic",
     "persian"         => "Persian",
     "english"         => "English",
+
     //Main Page
     "main_road_guide_title"         => "The Roadmap of \"Ya Hussien Way\" (Najaf-Karbala Road)",
     "main_road_guide_detail"        => "On your way to visit Imam Hussien in Arba'een, you can find and locate all the services you need to complete your walking trip. Those services include: Mawakeb (Centers for food, rest, and praying), Lost and Found offices, and all the known locations near to you. Also you can know the covered distance, the estimated time, and the numbers of electric line columns on the road.",
@@ -28,6 +30,7 @@ return [
     "main_centers_title"            => "Lost and Found",
     "main_centers_detail"           => "You can declare about any lost objects that you missed at your way to Karbala. Also you can declare a lost child or adult and the Lost and Found centers will receive your declaration and act according to it.",
     "main_btn_enter"                => "Entry",
+
     //Menu
     "menu_road_guid"         => "The Roadmap of \"Ya Hussien Way\" (Najaf-Karbala Road)",
     "menu_visitor_feqh"      => "The Fiqh of Al-Zayer (Jurisprudence of Imam Hussien's Visitors)",
@@ -35,4 +38,27 @@ return [
     "menu_visitor_reception" => "Hosting of Imam Husain's Visitors",
     "menu_majales_guide"     => "Majlis-e-Aza (consolation sessions)",
     "menu_centers"           => "Lost and Found",
+
+    //Visitor Feqh
+    "visitor_feqh_menu_show_all_questions" => "All Questions",
+    "visitor_feqh_menu_send_question"      => "Submit a question to the Al-Ajweba Al-Muyasare website",
+    "visitor_feqh_menu_app_masael"         => "Download Al-Ajweba Al-Muyasare App",
+    "visitor_feqh_menu_filter"             => "عرض حسب الصنف",
+
+    "visitor_feqh_title_questions"     => "فقه الزائر - جميع الاسئلة",
+    "visitor_feqh_title_app_masael"    => "فقه الزائر - الاجوبة الميسرة",
+
+    "visitor_feqh_ads_app_masael_text"   => "حمل الأن تطبيق الأجوبة الميسرة او زر الموقع الرسمي",
+    "visitor_feqh_ads_app_masael_detail" => "لرؤية المزيد من التفاصيل",
+    "visitor_feqh_ads_app_masael_btn"    => "اضغط هنا",
+
+    "visitor_feqh_questions_card_question"   => "السؤال : ",
+    "visitor_feqh_questions_card_answer"     => "الجواب : ",
+    "visitor_feqh_questions_pagination_prev" => "<< السابق",
+    "visitor_feqh_questions_pagination_next" => "التالي >>",
+
+    "visitor_feqh_app_masael_message_thanks" => "شكراً ايها الزائر الكريم لاستخدامك خدماتنا",
+    "visitor_feqh_app_masael_description"    => "بشرى سارة للمؤمنين، اصبح الآن الجواب على سؤالك الفقهي او العقائدي في متناول يدك فقط عن طريق استخدام موقع وتطبيق الأجوبة الميسرة وهذا المشروع من انتاج معهد تراث الأنبياء(عليهم السلام) وغايتهما هو مساعدة الناس في الاجابة على مسائلهم الشرعية التي تخص الفقه والعقائد بإشراف مجموعة من طلبة وأساتذة الحوزة العلمية في النجف الأشرف.",
+    "visitor_feqh_masael_website"            => "الموقع الرسمي للأجوبة الميسرة",
+    "visitor_feqh_masael_available_app_on"   => "يتوفر التطبيق الآن على",
 ];
