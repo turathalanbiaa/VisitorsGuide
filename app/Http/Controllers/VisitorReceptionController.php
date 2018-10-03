1c9170613b114e1d7176df284e13dacf9547a79d
FadhilDev <fadhilabbash@gmail.com>
2018-10-03 14:38:36 +0300

    check user reception loged in

--- app/Http/Controllers/VisitorReceptionController.php
+++ app/Http/Controllers/VisitorReceptionController.php
@@ -8,12 +8,10 @@ use Illuminate\Support\Facades\Session;
 class VisitorReceptionController extends Controller
 {
     public function index() {
-        session(['user_id' => 1]);
         return view('VisitorReception.index');
-
     }
     function my_session(){
-        return response()->json(session()->get('user_id'));
+        return response()->json(session()->get('USER_ID'));
     }
 
 }
