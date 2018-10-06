6704ace9c5a332b61ff949742f529ecf3e95491d
karrar <karrar@email.com>
2018-10-06 16:47:28 +0300

    add new paths

--- resources/views/items/cities.blade.php
+++ resources/views/items/cities.blade.php
@@ -3,5 +3,8 @@
 <select  id="input_city" name="city" class="form-control">
 
     <option selected value="0">نجف - كربلاء</option>
+    <option  value="1">حلة - كربلاء</option>
+    <option  value="2">بغداد - كربلاء</option>
+    <option  value="2">مسيب - كربلاء</option>
 
 </select>
